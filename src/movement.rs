f89e2df79fd069709ff414505870e23cb1284e7c
temanmd <temacxz@gmail.com>
2025-03-28 08:05:04 +0500

    Ignore invalid direction moves

--- src/movement.rs
+++ src/movement.rs
@@ -11,21 +11,38 @@ impl Plugin for MovementPlugin {
 
 fn movement_system(keyboard_input: Res<ButtonInput<KeyCode>>, mut game: ResMut<Game>) {
     let inputs = vec![KeyCode::KeyA, KeyCode::KeyD, KeyCode::KeyW, KeyCode::KeyS];
-    if game.direction_changed || !keyboard_input.any_pressed(inputs) {
+    if game.direction_changed
+        || !keyboard_input.any_pressed(inputs)
+        || !is_valid_move(&game.direction, &keyboard_input)
+    {
         return;
     }
 
-    if keyboard_input.just_pressed(KeyCode::KeyA) && game.direction != Direction::Right {
+    if keyboard_input.just_pressed(KeyCode::KeyA) {
         game.direction = Direction::Left;
-        game.direction_changed = true;
-    } else if keyboard_input.just_pressed(KeyCode::KeyD) && game.direction != Direction::Left {
+    } else if keyboard_input.just_pressed(KeyCode::KeyD) {
         game.direction = Direction::Right;
-        game.direction_changed = true;
-    } else if keyboard_input.just_pressed(KeyCode::KeyW) && game.direction != Direction::Down {
+    } else if keyboard_input.just_pressed(KeyCode::KeyW) {
         game.direction = Direction::Up;
-        game.direction_changed = true;
-    } else if keyboard_input.just_pressed(KeyCode::KeyS) && game.direction != Direction::Up {
+    } else if keyboard_input.just_pressed(KeyCode::KeyS) {
         game.direction = Direction::Down;
-        game.direction_changed = true;
     }
+    game.direction_changed = true;
+}
+
+fn is_valid_move(direction: &Direction, input: &ButtonInput<KeyCode>) -> bool {
+    match direction {
+        Direction::Left | Direction::Right => {
+            if input.just_pressed(KeyCode::KeyW) || input.just_pressed(KeyCode::KeyS) {
+                return true;
+            }
+        }
+        Direction::Up | Direction::Down => {
+            if input.just_pressed(KeyCode::KeyA) || input.just_pressed(KeyCode::KeyD) {
+                return true;
+            }
+        }
+    }
+
+    false
 }
